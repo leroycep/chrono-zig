fbc065ff403066586c278245076ce0cd2fe5954b
LeRoyce Pearson <me@leroycepearson.dev>
2021-10-07 05:21:16 -0600

    Add parseNaiveDateTime funciton to format

--- src/format.zig
+++ src/format.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const NaiveDate = @import("./date.zig").NaiveDate;
 const NaiveDateTime = @import("./datetime.zig").NaiveDateTime;
+const internals = @import("./internals.zig");
+const time = @import("./time.zig");
 
 pub const Specifier = enum {
     FullYear,
@@ -79,7 +81,7 @@ pub fn formatNaiveDateTime(writer: anytype, comptime format: []const u8, dt: Nai
     inline for (format) |fc| {
         if (next_char_is_specifier) {
             switch (fc) {
-                'F' => try writer.print("{d}-{d:0>2}-{d:0>2}", .{dt.year(), dt.month(), dt.day()}),
+                'F' => try writer.print("{d}-{d:0>2}-{d:0>2}", .{ dt.year(), dt.month(), dt.day() }),
                 'Y' => try writer.print("{d}", .{dt.year()}),
                 'm' => try writer.print("{d:0>2}", .{dt.month()}),
                 'd' => try writer.print("{d:0>2}", .{dt.day()}),
@@ -90,9 +92,9 @@ pub fn formatNaiveDateTime(writer: anytype, comptime format: []const u8, dt: Nai
                 'S' => try writer.print("{d:0>2}", .{dt.second()}),
 
                 // Combined time specifiers
-                'R' => try writer.print("{d:0>2}:{d:0>2}", .{dt.hour(), dt.minute()}),
-                'T' => try writer.print("{d:0>2}:{d:0>2}:{d:0>2}", .{dt.hour(), dt.minute(), dt.second()}),
-                
+                'R' => try writer.print("{d:0>2}:{d:0>2}", .{ dt.hour(), dt.minute() }),
+                'T' => try writer.print("{d:0>2}:{d:0>2}:{d:0>2}", .{ dt.hour(), dt.minute(), dt.second() }),
+
                 ';' => try writer.writeByte(':'),
                 else => @compileError("Invalid date format specifier " ++ fc),
             }
@@ -165,7 +167,126 @@ test "comptime format naive datetimes" {
         defer str.deinit();
 
         try formatNaiveDateTime(str.writer(), "%Y-%m-%d %H:%M:%S", case.datetime);
-        
+
         try std.testing.expectEqualSlices(u8, case.expected_string, str.items);
+    }
+}
+
+pub fn parseNaiveDateTime(comptime format: []const u8, dtString: []const u8) !NaiveDateTime {
+    var year: ?internals.YearInt = null;
+    var month: ?internals.MonthInt = null;
+    var day: ?internals.DayInt = null;
+    var hour: ?time.HoursInt = null;
+    var minute: ?time.MinutesInt = null;
+    var second: ?time.SecondsInt = null;
+
+    comptime var next_char_is_specifier = false;
+    var dt_string_idx: usize = 0;
+    inline for (format) |fc| {
+        if (next_char_is_specifier) {
+            switch (fc) {
+                'F' => {
+                    year = try parseDigits(internals.YearInt, dtString, &dt_string_idx, 4);
+                    month = try parseDigits(internals.MonthInt, dtString, &dt_string_idx, 2);
+                    day = try parseDigits(internals.DayInt, dtString, &dt_string_idx, 2);
+                },
+
+                'Y' => {
+                    std.debug.assert(year == null);
+                    // Read digits until: 1) there is four digits or 2) the next character is not a digit
+                    year = try parseDigits(internals.YearInt, dtString, &dt_string_idx, 4);
+                },
+                'm' => {
+                    std.debug.assert(month == null);
+                    // Read 2 digits or just 1 if the digit after is not a digit
+                    month = try parseDigits(internals.MonthInt, dtString, &dt_string_idx, 2);
+                },
+                'd' => {
+                    std.debug.assert(day == null);
+                    // Read 2 digits or just 1 if the digit after is not a digit
+                    day = try parseDigits(internals.DayInt, dtString, &dt_string_idx, 2);
+                },
+
+                // Time specifiers
+                'H' => {
+                    std.debug.assert(hour == null);
+                    hour = try parseDigits(time.HoursInt, dtString, &dt_string_idx, 2);
+                },
+                'M' => {
+                    std.debug.assert(minute == null);
+                    minute = try parseDigits(time.MinutesInt, dtString, &dt_string_idx, 2);
+                },
+                'S' => {
+                    std.debug.assert(second == null);
+                    second = try parseDigits(time.SecondsInt, dtString, &dt_string_idx, 2);
+                },
+
+                // Combined time specifiers
+                'R' => {
+                    std.debug.assert(hour == null and minute == null);
+                    hour = try parseDigits(time.HoursInt, dtString, &dt_string_idx, 2);
+                    minute = try parseDigits(time.MinutesInt, dtString, &dt_string_idx, 2);
+                },
+                'T' => {
+                    std.debug.assert(hour == null and minute == null and second == null);
+                    hour = try parseDigits(time.HoursInt, dtString, &dt_string_idx, 2);
+                    minute = try parseDigits(time.MinutesInt, dtString, &dt_string_idx, 2);
+                    second = try parseDigits(time.SecondsInt, dtString, &dt_string_idx, 2);
+                },
+
+                ';' => {
+                    if (dtString[dt_string_idx] != ':') return error.InvalidFormat;
+                    dt_string_idx += 1;
+                },
+                else => @compileError("Invalid date format specifier " ++ fc),
+            }
+            next_char_is_specifier = false;
+        } else {
+            if (fc == '%') {
+                next_char_is_specifier = true;
+            } else {
+                if (dtString[dt_string_idx] != fc) {
+                    return error.InvalidFormat;
+                }
+                dt_string_idx += 1;
+            }
+        }
+    }
+
+    std.debug.assert(year != null);
+    std.debug.assert(month != null);
+    std.debug.assert(day != null);
+    std.debug.assert(hour != null);
+    std.debug.assert(minute != null);
+
+    return NaiveDate.ymd(year.?, month.?, day.?).?.hms(hour.?, minute.?, second orelse 0).?;
+}
+
+fn parseDigits(comptime T: type, dtString: []const u8, idx: *usize, maxDigits: usize) !T {
+    const start_idx = idx.*;
+
+    if (!std.ascii.isDigit(dtString[start_idx])) return error.InvalidFormat;
+
+    idx.* += 1;
+    while (idx.* < start_idx + maxDigits and std.ascii.isDigit(dtString[idx.*])) : (idx.* += 1) {}
+
+    return try std.fmt.parseInt(T, dtString[start_idx..idx.*], 10);
+}
+
+test "comptime parse with comptime format string" {
+    const Case = struct {
+        string: []const u8,
+        expected_datetime: NaiveDateTime,
+    };
+
+    const cases = [_]Case{
+        .{ .string = "2021-02-18 17:00:00", .expected_datetime = NaiveDate.ymd(2021, 02, 18).?.hms(17, 00, 00).? },
+        .{ .string = "1970-01-01 00:00:00", .expected_datetime = NaiveDate.ymd(1970, 01, 01).?.hms(0, 0, 0).? },
+    };
+
+    for (cases) |case| {
+        const datetime = try parseNaiveDateTime("%Y-%m-%d %H:%M:%S", case.string);
+
+        try std.testing.expectEqual(case.expected_datetime, datetime);
     }
 }
