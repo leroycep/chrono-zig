3a425bd89358fb9114a8437c66028ec9950276b3
LeRoyce Pearson <me@leroycepearson.dev>
2022-01-04 18:28:35 -0700

    Correct WASM/JS timestamp conversion code

--- src/chrono.js
+++ src/chrono.js
@@ -1,13 +1,12 @@
+export default function getChronoEnv(getInstanceExports) {
+  const getMem = () => getInstanceExports().memory.buffer;
+  return {
+    getOffset(timestampMsInPtr, offsetOutPtr) {
+      const timestamp_ms = new BigInt64Array(getMem(), timestampMsInPtr, 1);
+      const offset_seconds = new BigInt64Array(getMem(), offsetOutPtr, 1);
 
-export default function getChronoEnv(getGlobalInstance) {
-    const getMem = () => getGlobalInstance().exports.memory.buffer;
-    return {
-        getOffset(timestampMsInPtr, offsetOutPtr) {
-            const timestamp_ms = new BigInt64Array(getMem(), timestampMsInPtr, 1);
-            const offset_seconds = new BigInt64Array(getMem(), offsetOutPtr, 1);
-
-            const date = new Date(Number(timestamp_ms[0]));
-            offset_seconds[0] = BigInt(date.getTimezoneOffset());
-        }
-    };
+      const date = new Date(Number(timestamp_ms[0]));
+      offset_seconds[0] = BigInt(-date.getTimezoneOffset()) * BigInt(60);
+    },
+  };
 }
