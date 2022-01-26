f224876d289d634b120ef97549539154f45b006e
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2022-01-26 10:49:31 +0000

    add missing timezoneToUtc fns & debug panic tzif.localTimeToUTC
    this allows compilation even if not everything is implemented yet
    debug panic instead to inform the user they try to use so far unimplemented functionality

--- src/timezone/tzif.zig
+++ src/timezone/tzif.zig
@@ -102,7 +102,8 @@ pub const TimeZone = struct {
     pub fn localTimeToUTC(this: @This(), localtime: i64) ?ConversionResult {
         _ = this;
         _ = localtime;
-        @compileError("Unimplemented");
+        std.debug.panic("Unimplemented", .{});
+        return null;
     }
 };
 
