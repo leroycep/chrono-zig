851e962250a08274cfd31ef43f3677f4934767da
geemili <opensource@geemili.xyz>
2023-12-02 22:02:39 -0700

    Update build.zig to actually run tests

--- build.zig
+++ build.zig
@@ -2,10 +2,17 @@ const std = @import("std");
 const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) void {
-    const tests = b.addTest(.{
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const test_exe = b.addTest(.{
         .root_source_file = .{ .path = "src/lib.zig" },
+        .target = target,
+        .optimize = optimize,
     });
 
-    const run_tests = b.step("test", "Test everything");
-    run_tests.dependOn(&tests.step);
+    const run_tests = b.addRunArtifact(test_exe);
+
+    const run_tests_step = b.step("test", "Test everything");
+    run_tests_step.dependOn(&run_tests.step);
 }
